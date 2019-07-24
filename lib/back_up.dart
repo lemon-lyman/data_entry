bc1b0abceed70e173cf31915daf9efc484c147e6
lemon-lyman <nicholaswest@utexas.edu>
2019-07-24 01:16:46 -0500

    back up:

--- lib/back_up.dart
+++ lib/back_up.dart
@@ -1,8 +1,53 @@
 import 'package:flutter/material.dart';
+import 'globals.dart' as globals;
+import 'package:intl/intl.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(MyApp());
+
+Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+  return directory.path;
+}
+
+Future<File> get _localFile async {
+  final path = await _localPath;
+  return File('$path/login_time.txt');
+}
+
+Future<File> writeDate(String date) async {
+  final file = await _localFile;
+  return file.writeAsString(date);
+}
+
+Future<DateTime> readDate() async {
+  try {
+    final file = await _localFile;
+    String contents = await file.readAsString();
+    return DateTime.parse(contents);
+  }catch (e) {
+//    return 0;
+    var a = 4;
+  }
+}
+
+Future<void> main() async {
+  globals.newLoginTime = DateTime.now(); // get login time
+  globals.oldLoginTime = await readDate();
+  writeDate(globals.newLoginTime.toString());
+//  read last login time
+//  write new login time
+//  if demarcation time was crossed:
+//      old data = read(new)
+//      new = blank
+//  else
+//      old = read(old)
+//      new = read(new)
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,43 +66,151 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+//void go2yesterday() {
+//  const num_rows = 8;
+//  var count_list = [0, 0, 0, 0, 0, 0, 0, 0];
+//  var now = DateTime.now();
+//  Navigator.of(context).push(
+//    MaterialPageRoute<void>(
+//      builder: (BuildContext context) {
+//        return Scaffold(
+//
+//          appBar: AppBar(
+//            title: Text(now.month.toString() + '/' + now.day.toString()),
+////        title: Text(new DateFormat('EEE, MM/DD').format()),
+//            actions: <Widget>[      // Add 3 lines from here...
+//              IconButton(icon: Icon(Icons.list), onPressed: go2yesterday),
+//            ],                      // ... to here.
+//          ),
+//
+//          body: new ListView(
+////        padding: const EdgeInsets.all(8.0),
+//            children: new List<Widget>.generate(num_rows, (index) {
+//              return new GridTile(
+//                child: new Container(
+//                  color: Colors.blue.shade200,
+//                  height: 89.5,
+//                  child: new Row(
+//                    mainAxisAlignment: MainAxisAlignment.center,
+//                    children: <Widget>[
+//
+//                      Expanded(
+//                        child: Container(
+//                            width: 200.0,
+//                            height: 200.0,
+//                            child: new RawMaterialButton(
+//                              shape: new CircleBorder(),
+//                              elevation: 0.0,
+//                              child: Icon(Icons.remove_circle_outline),
+//                              onPressed: (){setState((){count_list[index]--;});},
+//                            )
+//                        ),
+//                      ),
+//
+//                      Expanded(
+//                        child: Text(
+//                          count_list[index].toString(),
+//                          style: Theme.of(context).textTheme.display1,
+//                        ),
+//                      ),
+//                      Expanded(
+//                        child: Container(
+//                            width: 200.0,
+//                            height: 200.0,
+//                            child: new RawMaterialButton(
+//                              shape: new CircleBorder(),
+//                              elevation: 0.0,
+//                              child: Icon(Icons.add_circle_outline),
+//                              onPressed: (){setState((){count_list[index]++;});},
+//                            )
+//                        ),
+//                      ),
+//                      Expanded(
+//                        child: Text(
+//                          count_list[index].toString(),
+//                          style: Theme.of(context).textTheme.display1,
+//                        ),
+//                      ),
+//                    ],
+//                  ),
+//                ),
+//              );
+//
+//
+//
+//            }),
+//          ),
+//        );
+//      }
+//    )
+//  );
+//}
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static const num_rows = 8;
+  var count_list = [0, 0, 0, 0, 0, 0, 0, 0];
+  var now = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+      appBar: AppBar(
+//        title: Text(now.month.toString() + '/' + now.day.toString() + '/' + now.year.toString()),
+//        title: Text(globals.login_time.month.toString() + '/' + globals.login_time.day.toString() + '/' + globals.login_time.year.toString()),
+        title: Text(DateFormat('MM/dd/yy H:m:s').format(globals.oldLoginTime).toString() + ' ' + DateFormat('MM/dd/yyyy H:m:s').format(globals.newLoginTime).toString()),
+//        title: Text(new DateFormat('EEE, MM/DD').format()),
+        actions: <Widget>[      // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed:  (){}),
+        ],                      // ... to here.
+      ),
+
       body: new ListView(
-//        crossAxisCount: 2,
-        children: new List<Widget>.generate(8, (index) {
+//        padding: const EdgeInsets.all(8.0),
+        children: new List<Widget>.generate(num_rows, (index) {
           return new GridTile(
-            child: new Card(
+            child: new Container(
               color: Colors.blue.shade200,
+              height: 89.5,
               child: new Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text(
-                    '$_counter',
-                    style: Theme.of(context).textTheme.display1,
+
+                  Expanded(
+                    child: Container(
+                        width: 200.0,
+                        height: 200.0,
+                        child: new RawMaterialButton(
+                          shape: new CircleBorder(),
+                          elevation: 0.0,
+                          child: Icon(Icons.remove_circle_outline),
+                          onPressed: (){setState((){count_list[index]--;});},
+                        )
+                    ),
                   ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      setState(() {_counter++;});
-                    },
-                    tooltip: 'Increment',
-                    //              child: Icon(Icons.add),
-                    elevation: 0.7,
-                    disabledElevation: .5,
+
+                  Expanded(
+                    child: Text(
+                      count_list[index].toString(),
+                      style: Theme.of(context).textTheme.display1,
+                    ),
                   ),
-                  const SizedBox(height: 30),
-                  RaisedButton(
-                    onPressed: () {
-                      setState(() {_counter=0;});
-                    },
-                    child: const Text(
-                        'Reset',
-                        style: TextStyle(fontSize: 20)
+                  Expanded(
+                    child: Container(
+                        width: 200.0,
+                        height: 200.0,
+                        child: new RawMaterialButton(
+                          shape: new CircleBorder(),
+                          elevation: 0.0,
+                          child: Icon(Icons.add_circle_outline),
+                          onPressed: (){setState((){count_list[index]++;});},
+                        )
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      count_list[index].toString(),
+                      style: Theme.of(context).textTheme.display1,
                     ),
                   ),
                 ],
