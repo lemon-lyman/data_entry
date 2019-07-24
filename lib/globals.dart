a1c9679182bd3154ca8b2e38f11c079bcf88a62b
lemon-lyman <nicholaswest@utexas.edu>
2019-07-24 00:41:27 -0500

    Added login time && pseudo/sudo code

---
+++ lib/globals.dart
@@ -0,0 +1,3 @@
+library my_prj.globals;
+
+var login_time = DateTime.parse("1969-07-20 20:18:04Z");
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'globals.dart' as globals;
+import 'package:intl/intl.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  globals.login_time = DateTime.now(); // get login time
+//  read last login time
+//  if demarcation time was crossed:
+//      old data = read(new)
+//      new = blank
+//  else
+//      old = read(old)
+//      new = read(new)
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
 
@@ -22,85 +34,85 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-void go2yesterday() {
-  const num_rows = 8;
-  var count_list = [0, 0, 0, 0, 0, 0, 0, 0];
-  var now = DateTime.now();
-  Navigator.of(context).push(
-    MaterialPageRoute<void>(
-      builder: (BuildContext context) {
-        return Scaffold(
-
-          appBar: AppBar(
-            title: Text(now.month.toString() + '/' + now.day.toString()),
-//        title: Text(new DateFormat('EEE, MM/DD').format()),
-            actions: <Widget>[      // Add 3 lines from here...
-              IconButton(icon: Icon(Icons.list), onPressed: go2yesterday),
-            ],                      // ... to here.
-          ),
-
-          body: new ListView(
-//        padding: const EdgeInsets.all(8.0),
-            children: new List<Widget>.generate(num_rows, (index) {
-              return new GridTile(
-                child: new Container(
-                  color: Colors.blue.shade200,
-                  height: 89.5,
-                  child: new Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-
-                      Expanded(
-                        child: Container(
-                            width: 200.0,
-                            height: 200.0,
-                            child: new RawMaterialButton(
-                              shape: new CircleBorder(),
-                              elevation: 0.0,
-                              child: Icon(Icons.remove_circle_outline),
-                              onPressed: (){setState((){count_list[index]--;});},
-                            )
-                        ),
-                      ),
-
-                      Expanded(
-                        child: Text(
-                          count_list[index].toString(),
-                          style: Theme.of(context).textTheme.display1,
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                            width: 200.0,
-                            height: 200.0,
-                            child: new RawMaterialButton(
-                              shape: new CircleBorder(),
-                              elevation: 0.0,
-                              child: Icon(Icons.add_circle_outline),
-                              onPressed: (){setState((){count_list[index]++;});},
-                            )
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          count_list[index].toString(),
-                          style: Theme.of(context).textTheme.display1,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-
-
-
-            }),
-          ),
-        );
-      }
-    )
-  );
-}
+//void go2yesterday() {
+//  const num_rows = 8;
+//  var count_list = [0, 0, 0, 0, 0, 0, 0, 0];
+//  var now = DateTime.now();
+//  Navigator.of(context).push(
+//    MaterialPageRoute<void>(
+//      builder: (BuildContext context) {
+//        return Scaffold(
+//
+//          appBar: AppBar(
+//            title: Text(now.month.toString() + '/' + now.day.toString()),
+////        title: Text(new DateFormat('EEE, MM/DD').format()),
+//            actions: <Widget>[      // Add 3 lines from here...
+//              IconButton(icon: Icon(Icons.list), onPressed: go2yesterday),
+//            ],                      // ... to here.
+//          ),
+//
+//          body: new ListView(
+////        padding: const EdgeInsets.all(8.0),
+//            children: new List<Widget>.generate(num_rows, (index) {
+//              return new GridTile(
+//                child: new Container(
+//                  color: Colors.blue.shade200,
+//                  height: 89.5,
+//                  child: new Row(
+//                    mainAxisAlignment: MainAxisAlignment.center,
+//                    children: <Widget>[
+//
+//                      Expanded(
+//                        child: Container(
+//                            width: 200.0,
+//                            height: 200.0,
+//                            child: new RawMaterialButton(
+//                              shape: new CircleBorder(),
+//                              elevation: 0.0,
+//                              child: Icon(Icons.remove_circle_outline),
+//                              onPressed: (){setState((){count_list[index]--;});},
+//                            )
+//                        ),
+//                      ),
+//
+//                      Expanded(
+//                        child: Text(
+//                          count_list[index].toString(),
+//                          style: Theme.of(context).textTheme.display1,
+//                        ),
+//                      ),
+//                      Expanded(
+//                        child: Container(
+//                            width: 200.0,
+//                            height: 200.0,
+//                            child: new RawMaterialButton(
+//                              shape: new CircleBorder(),
+//                              elevation: 0.0,
+//                              child: Icon(Icons.add_circle_outline),
+//                              onPressed: (){setState((){count_list[index]++;});},
+//                            )
+//                        ),
+//                      ),
+//                      Expanded(
+//                        child: Text(
+//                          count_list[index].toString(),
+//                          style: Theme.of(context).textTheme.display1,
+//                        ),
+//                      ),
+//                    ],
+//                  ),
+//                ),
+//              );
+//
+//
+//
+//            }),
+//          ),
+//        );
+//      }
+//    )
+//  );
+//}
 
 class _MyHomePageState extends State<MyHomePage> {
   static const num_rows = 8;
@@ -112,10 +124,12 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
 
       appBar: AppBar(
-        title: Text(now.month.toString() + '/' + now.day.toString()),
+//        title: Text(now.month.toString() + '/' + now.day.toString() + '/' + now.year.toString()),
+//        title: Text(globals.login_time.month.toString() + '/' + globals.login_time.day.toString() + '/' + globals.login_time.year.toString()),
+        title: Text(DateFormat('MM/dd/yyyy h:m:s').format(globals.login_time).toString()),
 //        title: Text(new DateFormat('EEE, MM/DD').format()),
         actions: <Widget>[      // Add 3 lines from here...
-          IconButton(icon: Icon(Icons.list), onPressed: go2yesterday),
+          IconButton(icon: Icon(Icons.list), onPressed:  (){}),
         ],                      // ... to here.
       ),
 
